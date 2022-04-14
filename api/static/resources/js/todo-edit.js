a70e8cee8904d151d413be18ae1db504172497f8
Szoták Simon <szotak.simon16.com>
2022-04-14 15:34:54 +0200

    Todo edit almost done

--- api/static/resources/js/todo-edit.js
+++ api/static/resources/js/todo-edit.js
@@ -2,8 +2,9 @@ function showToast(message, success = false) {
     const toast = document.getElementById("snackbar")
     toast.className = success ? "show success" : "show"
     toast.innerHTML = message
-    setTimeout(function(){ toast.className = toast.className.replace("show", ""); }, 3000);
+    setTimeout(function() { toast.className = toast.className.replace("show", ""); }, 3000);
 }
+
 
 const editTodo = {
 
@@ -14,9 +15,9 @@ const editTodo = {
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({id})
+            body: JSON.stringify({ id })
         })
-        if(result.status !== 200) {
+        if (result.status !== 200) {
             showToast("Hiba történt!")
         } else {
             window.location.reload()
@@ -30,13 +31,62 @@ const editTodo = {
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({id, done: true})
+            body: JSON.stringify({ id, done: true })
         })
-        if(result.status !== 200) {
+        if (result.status !== 200) {
             showToast("Hiba történt!")
         } else {
             window.location.reload()
         }
-    }
+    },
 
+
+    important: false,
+
+    toggleImportant() {
+        this.important = !this.important
+        const indicator = document.querySelector("#todo-edit-important")
+        if (indicator) {
+            indicator.setAttribute("title", this.important ? "Megjelölés nem fontosként" : "Megjelölés fontosként")
+            indicator.innerHTML = this.important ? '🤩' : '😴'
+        }
+    },
+
+    validate() {
+
+        const todoText = document.querySelector("#todo-edit-title").value
+        if (todoText.length === 0) {
+            showToast("Add meg a feladat leírását!")
+            return
+        }
+
+        const categoryID = document.querySelector("#todo-edit-category").value
+
+
+        return {
+            important: this.important,
+            todo: todoText,
+            categoryID: categoryID,
+            important: this.important,
+        }
+    },
+
+    async create() {
+        const data = this.validate()
+        if (data) {
+            const result = await fetch(window.API_URL + "/todo", {
+                method: "PATCH",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+            if (result.status !== 200) {
+                showToast("Hiba történt!")
+            } else {
+                window.location.reload()
+            }
+        }
+    }
 }
